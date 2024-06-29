ecce5e1c1dff63e7f3f46d73e45d1ddb5f2b10f1
RannaEhab <rannafakhry@gmail.com>
2024-06-30 02:52:30 +0300

    Enhancing Login Page

--- src/pages/HomePage/Javascript/Login.js
+++ src/pages/HomePage/Javascript/Login.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { LockOutlined, UserOutlined } from "@ant-design/icons";
-import { Button, Checkbox, Form, Input } from "antd";
+import { Button, Checkbox, Form, Input, Row, Col } from "antd";
 import { useNavigate } from "react-router-dom";
 import { Layout } from "antd";
+
+const { Content } = Layout;
 
 const Login = ({ setIsAuthenticated }) => {
   const navigate = useNavigate();
@@ -16,57 +18,88 @@ const Login = ({ setIsAuthenticated }) => {
   };
 
   return (
-    <Layout className="first-layout">
-      <Form
-        style={{
-          padding: "24px",
-          marginTop: "15%",
-          width: "550px",
-          marginLeft: "25%",
-        }}
-        name="normal_login"
-        className="login-form"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-      >
-        <Form.Item
-          name="username"
-          rules={[{ required: true, message: "Please input your Username!" }]}
+    <Layout className="first-layout" style={{ minHeight: "100vh" }}>
+      <Content style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <Row
+          style={{
+            borderRadius: "8px",
+            boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
+            backgroundColor: "white",
+            width: "90%",
+            maxWidth: "1100px", // Adjusted max width for the entire card
+            overflow: "hidden"
+          }}
         >
-          <Input
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="Username"
-          />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: "Please input your Password!" }]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Form.Item name="remember" valuePropName="checked" noStyle>
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
-          <a className="login-form-forgot" href="">
-            Forgot password
-          </a>
-        </Form.Item>
-        <Form.Item>
-          <Button
-            type="primary"
-            htmlType="submit"
-            className="login-form-button"
-          >
-            Log in
-          </Button>
-          Or <a href="">register now!</a>
-        </Form.Item>
-      </Form>
+          <Col span={12} style={{ padding: "5%" }}> {/* Use percentage padding */}
+            <Form
+              name="normal_login"
+              className="login-form"
+              initialValues={{ remember: true }}
+              onFinish={onFinish}
+              style={{ width: "100%" }}
+            >
+              <h2 style={{ textAlign: "center", color: "#101251", marginBottom: "4%" }}>SIGN <span style={{ color: "#1890ff" }}>IN</span></h2>
+              <Form.Item
+                name="username"
+                rules={[{ required: true, message: "Please input your Username!" }]}
+                style={{ marginBottom: "2%" }}
+              >
+                <Input
+                  size="large"
+                  prefix={<UserOutlined className="site-form-item-icon" />}
+                  placeholder="Username"
+                />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                rules={[{ required: true, message: "Please input your Password!" }]}
+                style={{ marginBottom: "4%" }}
+              >
+                <Input
+                  size="large"
+                  prefix={<LockOutlined className="site-form-item-icon" />}
+                  type="password"
+                  placeholder="Password"
+                />
+              </Form.Item>
+              <Form.Item style={{ marginBottom: "4%" }}>
+                <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                  <Checkbox>Stay signed in</Checkbox>
+                  <a className="login-form-forgot" href="">Forgot Password?</a>
+                </div>
+              </Form.Item>
+              <Form.Item style={{ marginBottom: "4%" }}>
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                  <Button
+                    style={{ width: "100%", maxWidth: "200px", backgroundColor: "#1890ff", borderColor: "#1890ff" }}
+                    size="large"
+                    type="primary"
+                    htmlType="submit"
+                    className="login-form-button"
+                  >
+                    Login
+                  </Button>
+                </div>
+              </Form.Item>
+            </Form>
+          </Col>
+          <Col span={12} style={{ padding: "0" }}> {/* Adjusted width */}
+            <video
+              autoPlay
+              loop
+              muted
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+              }}
+            >
+              <source src="/SupplyChain.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </Col>
+        </Row>
+      </Content>
     </Layout>
   );
 };
